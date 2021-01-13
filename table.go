90d2c461f481635c358eb05a1644406ae4d189bd
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-01-13 01:33:39 -0500

    better table functionality

--- table.go
+++ table.go
@@ -12,8 +12,9 @@ import (
 
 //TableDir is a directory containing all the tables in a database
 type TableDir struct {
-	fs   *FS
-	name string
+	fs    *FS
+	name  string
+	query *File
 }
 
 var _ fs.Node = (*TableDir)(nil)
@@ -27,10 +28,17 @@ func (d *TableDir) Attr(ctx context.Context, a *fuse.Attr) error {
 
 func (d *TableDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	log.Println("[d *TableDir Lookup] name=", name)
+	if name == "query" {
+		d.query = &File{}
+		d.query.content.Store("asdf")
+	}
 	return nil, syscall.ENOENT
 }
 
 func (d *TableDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 	log.Println("[d *TableDir ReadDirAll]")
-	return nil, nil
+	// make a file for querying data
+	content := make([]fuse.Dirent, 0)
+	content = append(content, fuse.Dirent{Inode: d.fs.common.getInode(), Name: "query", Type: fuse.DT_File})
+	return content, nil
 }
